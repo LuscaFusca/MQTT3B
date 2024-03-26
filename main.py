e68d6d1d40277731a5af9b83b1f7b51e1a41d18e
LuscaFusca <lucasgomes_2006@hotmail.com>
2024-03-26 09:26:20 -0300

    26/03

--- main.py
+++ main.py
@@ -5,11 +5,17 @@ def ao_conectar(client, userdata, flags, rc):
 
 def ao_receber(client, userdata, msg):
     print("{} --- {}".format(msg.topic, str(msg.payload)))
-
+    if "DESLIGAR" in str(msg.payload).upper():
+        print("DESLIGANDO O SISTEMA")
 cliente = mqtt.Client()
 
 cliente.on_connect = ao_conectar
 cliente.on_message = ao_receber
 cliente.connect("broker.hivemq.com", 1883, 60)
 cliente.subscribe("aula3b")
-cliente.loop_forever()
+#cliente.loop_forever()
+
+cliente.loop_start()
+while True:
+    cliente.publish("aula3b", input("Envie uma mensagem"))
+cliente.loop_finish()
